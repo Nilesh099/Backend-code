72e7d5595faf4d7d6f213248ec846d9652a4dff8
Nilesh Mahajan <nrmahajan99@gmail.com>
2025-08-21 17:14:29 +0530

    Adds post creation functionality.

--- index.js
+++ index.js
@@ -41,7 +41,12 @@ app.get("/posts/new", (req, res) => {
   res.render("new.ejs");
 });
 
+app.post("/posts", (req, res) => {
+  let {username, content}= req.body;
+  posts.push({username,content})
+  res.redirect("/posts");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-  
